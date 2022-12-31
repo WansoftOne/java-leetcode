6c9bac06aa5bba1dbd5bb460d51ca91bab1a84db
Wansoft <wansoft.developer@gmail.com>
2022-12-30 22:18:00 -0600

    Refactor

---
+++ src/main/java/mx/wansoft/FlippingAnImage.java
@@ -0,0 +1,65 @@
+package mx.wansoft;
+
+/**
+ * 
+ * Given an n x n binary matrix image, flip the image horizontally, then invert
+ * it, and return the resulting image.
+ * 
+ * To flip an image horizontally means that each row of the image is reversed.
+ * 
+ * For example, flipping [1,1,0] horizontally results in [0,1,1].
+ * To invert an image means that each 0 is replaced by 1, and each 1 is replaced
+ * by 0.
+ * 
+ * For example, inverting [0,1,1] results in [1,0,0].
+ * 
+ * 
+ * Example 1:
+ * 
+ * Input: image = [[1,1,0],[1,0,1],[0,0,0]]
+ * Output: [[1,0,0],[0,1,0],[1,1,1]]
+ * Explanation: First reverse each row: [[0,1,1],[1,0,1],[0,0,0]].
+ * Then, invert the image: [[1,0,0],[0,1,0],[1,1,1]]
+ * Example 2:
+ * 
+ * Input: image = [[1,1,0,0],[1,0,0,1],[0,1,1,1],[1,0,1,0]]
+ * Output: [[1,1,0,0],[0,1,1,0],[0,0,0,1],[1,0,1,0]]
+ * Explanation: First reverse each row:
+ * [[0,0,1,1],[1,0,0,1],[1,1,1,0],[0,1,0,1]].
+ * Then invert the image: [[1,1,0,0],[0,1,1,0],[0,0,0,1],[1,0,1,0]]
+ * 
+ * 
+ * Constraints:
+ * 
+ * n == image.length
+ * n == image[i].length
+ * 1 <= n <= 20
+ * images[i][j] is either 0 or 1.
+ * 
+ */
+public class FlippingAnImage {
+    class Solution {
+        public int[][] flipAndInvertImage(int[][] image) {
+            int n = image.length;
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    int left = image[i][j];
+                    int rigthIndex = (n - 1) - j;
+                    int rigth = image[i][rigthIndex];
+                    if (j < rigthIndex) {
+                        // normal case, swap values
+                        image[i][j] = Math.abs(rigth - 1);
+                        image[i][(n - 1) - j] = Math.abs(left - 1);
+                    } else if (j == rigthIndex) {
+                        // it ends we dont need to swap values
+                        image[i][j] = Math.abs(rigth - 1);
+                        break;
+                    } else {
+                        break;
+                    }
+                }
+            }
+            return image;
+        }
+    }
+}
